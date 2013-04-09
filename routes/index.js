08b319940e48688ec2c9109badf25434ebc2805a
Ian Obermiller <ian@obermillers.com>
2013-04-09 09:51:14 -0700

    Initial commit.

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var request = require('request');
+var FeedParser = require('feedparser');
+
+exports.index = function(req, res){
+    var data = { title: 'Morning Brew Mobile', articles: [] };
+    request('http://blog.cwa.me.uk/feed/')
+        .pipe(new FeedParser())
+        .on('error', function(error) {
+            console.log('error', error);
+        })
+        .on('meta', function(meta) {
+            data.date = meta.date;
+        })
+        .on('article', function(article) {
+            var art = {};
+            art.title = article.title;
+            art.html = article.description;
+            art.html = art.html.replace(/<a href="(.*?)">.*?<\/a>/g, '<a class="text-link" href="http://viewtext.org/article?url=$1">Text</a> $&')
+            data.articles.push(art);
+        })
+        .on('end', function() {
+            res.render('index', data)
+        });
+};
